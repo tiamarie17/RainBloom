6285bd251896d7130044a09240b25cb150609faf
Tia Theisen <tiatheisen@gmail.com>
2022-11-14 13:43:42 -0600

    render experiment map

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -6,11 +7,24 @@ import React from 'react';
 // or even care what the redux state is
 
 function InfoPage() {
+
+  
   return (
-    <div className="container">
-      <p>Info Page</p>
-    </div>
+     <div id="map">
+      <MapContainer 
+        center={[37.0902, -95.7129]} 
+        zoom={3} 
+        zoomControl={false} 
+        style={{ height: '100vh', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+      </div>
   );
 }
+
 
 export default InfoPage;
