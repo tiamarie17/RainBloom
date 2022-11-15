71ebb50fbe6e695adeb033e4be1641db24a57d73
Tia Theisen <tiatheisen@gmail.com>
2022-11-15 11:34:12 -0600

    update error message in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -121,7 +121,7 @@ function App() {
 
           {/* If none of the other routes matched, we will show a 404. */}
           <Route>
-            <h1>404</h1>
+            <h1>404- Oops! This page wasn't found.</h1>
           </Route>
         </Switch>
         <Footer />
