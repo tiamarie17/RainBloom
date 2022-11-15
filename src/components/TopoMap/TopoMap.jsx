8302f89d8d6d67b19320598fb362d5408eed4ab0
Tia Theisen <tiatheisen@gmail.com>
2022-11-15 01:50:30 -0600

    render map in topomap route

--- src/components/TopoMap/TopoMap.jsx
+++ src/components/TopoMap/TopoMap.jsx
@@ -1,46 +1,26 @@
 import React, {useState, useEffect} from 'react';
-import { MapContainer, TileLayer, useMap, Marker, Popup} from 'react-leaflet';
+import { MapContainer, TileLayer, useMap, useMapEvents, Marker, Popup} from 'react-leaflet';
 import { Icon } from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 
-
 function TopoMap(){
+      return (
+        <>
+        <MapContainer 
+          center={[37.0902, -95.7129]} 
+          zoom={3} 
+          zoomControl={false} 
+          style={{ height: '100vh', width: '100%' }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+        </>
+        
+    );
 
-    function LocationMarker() {
-
-        const [position, setPosition] = useState(null);
-    
-        const map = useMap();
-    
-        useEffect(() => {
-          map.locate().on("locationfound", function (e) {
-            setPosition(e.latlng);
-            map.flyTo(e.latlng, map.getZoom());
-          });
-        }, []);
-    
-        return position === null ? null : (
-          <Marker position={position} icon={visitorIcon}>
-            <Popup>You are here</Popup>
-          </Marker>
-        );
-      }
-
-  return (
-      <>
-      <MapContainer 
-        center={[37.0902, -95.7129]} 
-        zoom={3} 
-        zoomControl={false} 
-        style={{ height: '100vh', width: '100%' }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
-      </>
+  }
       
-  );
-}
 export default TopoMap;
