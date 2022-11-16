d7672270338aa6e67b9bed37b61ab2e6e3260a78
Tia Theisen <tiatheisen@gmail.com>
2022-11-16 17:11:06 -0600

    checkbox form allowing multiple boxes to be checked or unchecked

--- src/components/PlantList/PlantList.jsx
+++ src/components/PlantList/PlantList.jsx
@@ -14,10 +14,18 @@ function PlantList(){
 
 return(
     <>
-    <h1>Choose Plants For Your Garden</h1>
+    <h3>Choose Plants For Your Garden:</h3>
     <FormGroup>
-      <FormControlLabel control={<Checkbox checked={checked} onChange ={handleChange} inputProps={{'aria-label': 'controlled'}} />} label="Hummingbirds" />
-      
+    <FormControlLabel control={<Checkbox checked={checked[0] && checked[1]}
+      indeterminate={checked[0] !== checked[1]}onChange ={handleChange} inputProps={{'aria-label': 'controlled'}} />} label="Butterflies" />
+      <FormControlLabel control={<Checkbox checked={checked[0] && checked[1]}
+      indeterminate={checked[0] !== checked[1]} onChange ={handleChange} inputProps={{'aria-label': 'controlled'}} />} label="Hummingbirds" />
+      <FormControlLabel control={<Checkbox checked={checked[0] && checked[1]}
+      indeterminate={checked[0] !== checked[1]} onChange ={handleChange} inputProps={{'aria-label': 'controlled'}} />} label="Birds" />
+      <FormControlLabel control={<Checkbox checked={checked[0] && checked[1]}
+      indeterminate={checked[0] !== checked[1]} onChange ={handleChange} inputProps={{'aria-label': 'controlled'}} />} label="Pollinators" />
+      <FormControlLabel control={<Checkbox checked={checked[0] && checked[1]}
+      indeterminate={checked[0] !== checked[1]} onChange ={handleChange} inputProps={{'aria-label': 'controlled'}} />} label="Deer Resistant" />
     </FormGroup>
     </>
     );
