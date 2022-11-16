d5e9fa798bc617dfc164be62de3a149bea9d504f
Tia Theisen <tiatheisen@gmail.com>
2022-11-16 14:18:15 -0600

    add delete saga and import it into root saga

---
+++ src/redux/sagas/delete.saga.js
@@ -0,0 +1,27 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* removeImage(action) {
+
+    try {
+        console.log('in removeImage', action);
+        const deletedImage = action.payload
+
+        console.log('deletedImage is', deletedImage);
+        // POST the payload to the server
+        yield axios.delete(`/api/gallery/${deletedImage}`);
+
+        // trigger a GET request
+        yield put({
+            type: 'FETCH_GALLERY'
+        });
+    } catch (err) {
+        console.log('delete failed, error is', err);
+    }
+}
+
+function* deleteSaga() {
+    yield takeLatest('REMOVE_IMAGE', removeImage);
+}
+
+export default deleteSaga;
