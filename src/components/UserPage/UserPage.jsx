4c4785325d965811f1e8c3c0412f3ca1ecbd1c0c
Tia Theisen <tiatheisen@gmail.com>
2022-11-15 20:13:41 -0600

    add buttons to user page with onClick functions

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {useHistory} from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+
+  const history = useHistory();
+
+  const goToFaq = () =>{
+      console.log('in goToFaq');
+      history.push('/faq');
+  }
+
+  const buildRainGarden = () =>{
+    console.log('in buildRainGarden');
+    history.push('/plant');
+}
+
+const goToMyGarden = () =>{
+  console.log('in goToMyGarden');
+  history.push('/mygarden');
+}
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      {/* <LogOutButton className="btn" /> */}
+      <button onClick = {goToFaq}>FAQs</button>
+      <button onClick= {buildRainGarden}>Build a Rain Garden</button>
+      <button onClick = {goToMyGarden}>My Garden</button>
     </div>
   );
 }
