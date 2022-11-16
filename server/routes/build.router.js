90641131ee04a3261c18e3ee8e042cf9d6a10d0f
Tia Theisen <tiatheisen@gmail.com>
2022-11-16 15:47:47 -0600

    plantlist component created

---
+++ server/routes/build.router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {rejectUnauthenticated} = require('../modules/authentication-middleware');
+
+router.get('/', (req, res) => {
+    if (req.isAuthenticated()) {
+      console.log('/build GET route');
+      console.log('is authenticated?', req.isAuthenticated());
+      console.log('user', req.user);
+      
+     //query text
+
+      pool.query(queryText, [req.user.id]).then((result) => {
+          res.send(result.rows);
+      }).catch((error) => {
+          console.log(error);
+          res.sendStatus(500); 
+      });
+  
+  }else {
+      res.sendStatus(403);
+  }
+    
+  });
---
+++ src/components/PlantList/PlantList.jsx
@@ -0,0 +1,10 @@
+
+
+
+function PlantList(){
+    return(
+        <>
+        </>
+    );
+}
+export default PlantList;
