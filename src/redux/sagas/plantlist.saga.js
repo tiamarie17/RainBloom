a443f4c4b4eb283863f6bf676d2b8604d47bda4a
Tia Theisen <tiatheisen@gmail.com>
2022-11-18 11:39:25 -0600

    update plantlist saga

--- src/redux/sagas/plantlist.saga.js
+++ src/redux/sagas/plantlist.saga.js
@@ -1,36 +1,33 @@
 import { put, takeLatest } from "@redux-saga/core/effects";
 import axios from "axios";
 
+
+
 // POST request to send search input to database
 function* sendSearchInput(action) {
+
     console.log('in sendSearchInput saga, action.payload is', action.payload);
-    let searchQuery=action.payload;
     
-    // try {
-    //     response = yield axios.post('/api/search', {data: searchQuery});
-    //     console.log(response)
-    //     yield put({ type:'FETCH_PLANTS'});
-    // } catch (err) {
-    //     console.log('Error with posting new item to plantlist', err);
-    // }
+    let searchQuery=action.payload;
+
     axios.post('/api/search', {
         data: searchQuery
       })
-      .then(function (response) {
-        console.log(response);
-        dispatch({
-            type: 'STORE_RESULTS',
-            payload: response.data
-        })
-      })
-      .catch((error) => {
-        console.log('Error with posting new item to plantlist', error);
-      });
+        
+    try {
+        response = yield axios.post('/api/search', {data: searchQuery});
+        console.log('response is', response);
+
+        yield put({ type:'STORE_PLANTS', payload: response.data});
+    } catch (err) {
+        console.log('Error with posting new item to plantlist', err);
+    }
+   
 }
 
 function* plantListSaga() {
     yield takeLatest('SEND_SEARCH_INPUT', sendSearchInput);
-    // yield takeLatest('FETCH_PLANTS', fetchPlants);
+
 }
 
 export default plantListSaga;
