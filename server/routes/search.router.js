49258dd3fe53e2e7bf5590c96d788e0cded3b823
Tia Theisen <tiatheisen@gmail.com>
2022-11-17 22:32:18 -0600

    search router sql working

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -1,49 +1,63 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-const {rejectUnauthenticated} = require('../modules/authentication-middleware');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 // GET router to fetch search results
 router.get('/', (req, res) => {
 
     if (req.isAuthenticated()) {
-      console.log('/search GET route');
-      console.log('is authenticated?', req.isAuthenticated());
-      console.log('user', req.user);
-      
-     //query text
+        console.log('/search GET route');
+        console.log('is authenticated?', req.isAuthenticated());
+        console.log('user', req.user);
 
-      pool.query(queryText, [req.user.id]).then((result) => {
-          res.send(result.rows);
-      }).catch((error) => {
-          console.log(error);
-          res.sendStatus(500); 
-      });
-  
-  }else {
-      res.sendStatus(403);
-  }
-    
-  });
+        //query text
 
-  //POST router to send search results to database
-  //Conditional rendering to look for matching search results
+        pool.query(queryText, [req.user.id]).then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
 
-  router.post('/', rejectUnauthenticated, function (req, res) {
-    console.log('in /search POST router');
-    console.log('req.body is', req.body);
-
-
-    //  let sqlText;
-
-     pool.query()
-     .then((result)=>{
-         res.sendStatus(200);
-     })
-     .catch((err)=> {
-         console.log('in search POST router error, error is', err);
-     })
+    } else {
+        res.sendStatus(403);
+    }
 
 });
 
-  module.exports = router;
+//POST router to send search results to database
+//Conditional rendering to look for matching search results
+
+router.post('/', rejectUnauthenticated, function (req, res) {
+    console.log('in /search POST router');
+    console.log('req.body.data is', req.body.data);
+
+    let soil = req.body.data.soil_type;
+    let sunlight = req.body.data.sunlight_amount;
+    let goals = req.body.data.goals;
+
+    let goalString = (goals.map(goal => (goal + " = TRUE")));
+    console.log(goalString);
+
+    goalString = goalString.join(" OR ");
+    goalString = `(${goalString});`
+    console.log(goalString);
+
+    let original = `SELECT * FROM plants WHERE soil_type = \'${soil}\' AND sunlight_amount = \'${sunlight}\'`;
+
+    let sqlText = `${original} AND ${goalString}`
+
+    console.log('sqlText is', sqlText);
+
+    pool.query(sqlText)
+        .then((result) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('in search POST router error, error is', err);
+        })
+
+});
+
+module.exports = router;
