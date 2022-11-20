5681552d792390ff1d99d8de811287688da622de
Tia Theisen <tiatheisen@gmail.com>
2022-11-20 15:28:49 -0600

    update search router to include placeholders, fix security issue with variable interpolation

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -3,49 +3,31 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-// GET router to fetch search results
-router.get('/', (req, res) => {
 
-    if (req.isAuthenticated()) {
-        console.log('/search GET route');
-        console.log('is authenticated?', req.isAuthenticated());
-        console.log('user', req.user);
-
-        //query text
-
-        pool.query(queryText, [req.user.id])
-        
-        .then((result) => {
-            res.send(result.rows);
-        }).catch((error) => {
-            console.log(error);
-            res.sendStatus(500);
-        });
-
-    } else {
-        res.sendStatus(403);
-    }
-
-});
-
-//POST router to send search results to database
+//POST router to send  and retrieve search results to database
 
 router.post('/', rejectUnauthenticated, function (req, res) {
     console.log('in /search POST router');
     console.log('req.body.data is', req.body.data);
-
-    let soil = req.body.data.soil_type;
     let sunlight = req.body.data.sunlight_amount;
-    let goals = req.body.data.goals;
+    let soil = req.body.data.soil_type;
 
-    let goalString = (goals.map(goal => (goal + " = TRUE")));
+
+    //mapping through goal array and removing strings so that it is a boolean value
+    let goals = req.body.data.goals.map(goal => goal.replace('\'', ""));
+
+    //declaring index value, adding placeholders starting at $3 for goals in array
+    //incrementing index so that number of placeholders is reponsive to number of items in array
+    let index = 3
+    let goalString = (goals.map(goal => ("$" + index++  + " = TRUE")));
     console.log(goalString);
 
+    //
     goalString = goalString.join(" OR ");
     goalString = `(${goalString});`
     console.log(goalString);
 
-    let baseSql = `SELECT * FROM plants WHERE soil_type = \'${soil}\' AND sunlight_amount = \'${sunlight}\'`;
+    let baseSql = `SELECT * FROM plants WHERE soil_type = $1 AND sunlight_amount = $2`;
 
     // sql = `
     // ///......
@@ -63,7 +45,9 @@ router.post('/', rejectUnauthenticated, function (req, res) {
 
     console.log('sqlText is', sqlText);
 
-    pool.query(sqlText)
+    let sqlParams = [soil, sunlight].concat(goals);
+
+    pool.query(sqlText, sqlParams)
         .then((result) => {
             res.status(200).send(result.rows);
             
