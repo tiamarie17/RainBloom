be7d32f1b6ee456e2ec85f97a1efa809014628d2
Tia Theisen <tiatheisen@gmail.com>
2022-11-20 16:45:33 -0600

    update search router to make secure, add if statements for goal array

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -9,25 +9,37 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 router.post('/', rejectUnauthenticated, function (req, res) {
     console.log('in /search POST router');
     console.log('req.body.data is', req.body.data);
+
+    //declaring variables for sqlParams
     let sunlight = req.body.data.sunlight_amount;
     let soil = req.body.data.soil_type;
+    let goals = req.body.data.goals;
+    console.log('goals is', goals);
 
 
-    //mapping through goal array and removing strings so that it is a boolean value
-    let goals = req.body.data.goals.map(goal => goal.replace('\'', ""));
 
-    //declaring index value, adding placeholders starting at $3 for goals in array
-    //incrementing index so that number of placeholders is reponsive to number of items in array
-    let index = 3
-    let goalString = (goals.map(goal => ("$" + index++  + " = TRUE")));
-    console.log(goalString);
+    //creating base SQL text with placeholders for soil and sunlight
+    let sqlText = `SELECT * FROM plants WHERE soil_type = $1 AND sunlight_amount = $2`;
 
-    //
-    goalString = goalString.join(" OR ");
-    goalString = `(${goalString});`
-    console.log(goalString);
+    let index =3;
+    //adding onto SQL string if the goals array contains certain goals
+    if (goals.includes('Butterflies')){
+        sqlText += ' OR Butterflies = True';
+    }
 
-    let baseSql = `SELECT * FROM plants WHERE soil_type = $1 AND sunlight_amount = $2`;
+    if (goals.includes('Hummingbirds')){
+        sqlText += ' OR Hummingbirds = True';
+    }
+    if (goals.includes('Pollinators')){
+        sqlText += ' OR Pollinators = True';
+    }
+
+    if (goals.includes('deerresistant')){
+        sqlText += ' OR deerresistant = True';
+    }
+    if (goals.includes('Birds')){
+        sqlText += ' OR Birds = True';
+    }
 
     // sql = `
     // ///......
@@ -36,7 +48,7 @@ router.post('/', rejectUnauthenticated, function (req, res) {
     // `
     // params = [goal.contains('Butterflies'), goals.contains('bananas')];
 
-    let sqlText = `${baseSql} AND ${goalString}`
+    // let sqlText = `${baseSql} AND ${goalString}`
 
     // if (goals.contains('Butterflies')) {
     //     sqlText += 'OR Butterflies = True'
@@ -45,7 +57,8 @@ router.post('/', rejectUnauthenticated, function (req, res) {
 
     console.log('sqlText is', sqlText);
 
-    let sqlParams = [soil, sunlight].concat(goals);
+    let sqlParams = [soil, sunlight];
+    console.log('sqlParams is', sqlParams);
 
     pool.query(sqlText, sqlParams)
         .then((result) => {
