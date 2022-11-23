049d282a423a85ca4e1b4eef1e7b1421cb479652
Tia Theisen <tiatheisen@gmail.com>
2022-11-22 21:50:47 -0600

    render mapbox map as vector tile layer in leaflet map

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -18,8 +18,8 @@ function Location(){
         <>
         <button onClick={size}>Go to Rain Garden Size</button>
         <MapContainer 
-          center={[37.0902, -95.7129]} 
-          zoom={3} 
+          center={[44.9773, -93.2655]} 
+          zoom={5} 
           zoomControl={false} 
           style={{ height: '100vh', width: '100%' }}
         >
@@ -29,26 +29,36 @@ function Location(){
             We also want to pass the checked prop to whichever map tile we want
             displayed as the default:
           */}
-          <LayersControl.BaseLayer checked name="Basic Map">
-            <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-          </LayersControl.BaseLayer>
-          {/* 
-            Add the second maptile:
-          */}
-          <LayersControl.BaseLayer name="Topo Map">
+
+          <LayersControl.BaseLayer checked name="Topo Map 1">
             <TileLayer
               attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
               url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
             />
           </LayersControl.BaseLayer>
+
+          <LayersControl.BaseLayer name="Topo Map 2">
+            {/* <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            /> */}
+
+            <VectorTileLayer
+                  styleUrl="mapbox://styles/tt17/classs3qb000515puopf36dp7"
+                  accessToken="pk.eyJ1IjoidHQxNyIsImEiOiJjbGFma2ZseWMwY25jM3BvN2pwN3hhNHZ0In0.gzQmSpyAwkDAOw_uTit6kw"
+                />
+          </LayersControl.BaseLayer>
+
+
+          <LayersControl.BaseLayer name="Base Map">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          
+      
         </LayersControl>
-           <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
          
         </MapContainer>
       
