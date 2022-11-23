8b2ff09971ad67dd90d766a58f3595ac90caec09
Tia Theisen <tiatheisen@gmail.com>
2022-11-23 10:26:26 -0600

    refactor mapcontainer code, create layers component

---
+++ src/components/Location/Layers.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { TileLayer, LayersControl } from 'react-leaflet';
+import VectorTileLayer from 'react-leaflet-vector-tile-layer';
+
+function Layers(){
+    return(
+    <>
+            <LayersControl position="topright">
+  
+
+  <LayersControl.BaseLayer checked name="Topo Map 1">
+    <TileLayer
+      attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+      url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+    />
+  </LayersControl.BaseLayer>
+
+  <LayersControl.BaseLayer name="Topo Map 2">
+  {/* Import custom MapBox style in a vector tyle from MapBox */}
+    <VectorTileLayer
+          styleUrl="mapbox://styles/tt17/classs3qb000515puopf36dp7"
+          accessToken={process.env.REACT_APP_MAPBOX_KEY}
+        />
+  </LayersControl.BaseLayer>
+
+
+  <LayersControl.BaseLayer name="Base Map">
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  </LayersControl.BaseLayer>
+  
+
+</LayersControl>
+    </>);
+}
+
+export default Layers;
--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,15 +1,12 @@
-
-
 import React, {useEffect} from 'react';
-import { MapContainer, TileLayer, useMap, LayersControl} from 'react-leaflet';
+import { MapContainer, useMap} from 'react-leaflet';
 import {useHistory} from 'react-router-dom';
-import VectorTileLayer from 'react-leaflet-vector-tile-layer';
 import { OpenStreetMapProvider, GeoSearchControl } from 'leaflet-geosearch';
 import "leaflet/dist/leaflet.css";
 import "leaflet-geosearch/dist/geosearch.css";
 import L from 'leaflet';
 import { Input } from '@mui/material';
-
+import Layers from './Layers';
 
 
 
@@ -64,34 +61,7 @@ function Location(){
           zoomControl={false} 
           style={{ height: '100vh', width: '100%' }}
         >
-           <LayersControl position="topright">
-  
-
-          <LayersControl.BaseLayer checked name="Topo Map 1">
-            <TileLayer
-              attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-              url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-            />
-          </LayersControl.BaseLayer>
-
-          <LayersControl.BaseLayer name="Topo Map 2">
-          {/* Import custom MapBox style in a vector tyle from MapBox */}
-            <VectorTileLayer
-                  styleUrl="mapbox://styles/tt17/classs3qb000515puopf36dp7"
-                  accessToken={process.env.REACT_APP_MAPBOX_KEY}
-                />
-          </LayersControl.BaseLayer>
-
-
-          <LayersControl.BaseLayer name="Base Map">
-            <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-          </LayersControl.BaseLayer>
-          
-      
-        </LayersControl>
+        <Layers/>
 
         <LeafletGeoSearch/>
          
